6dc00be319d426b3daaa596bbaf2021931ab7bc2
maksimanom <maksymKozoriz@gmail.com>
2020-06-16 14:53:30 +0300

    theming done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CheckIcon from "@material-ui/icons/Check";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      justifyContent: "center",
+      marginTop: 30,
+    },
+    table: {
+      "& .headingRow": {
+        backgroundColor: "#f0f0f0",
+        display: "flex",
+        "& .head-cell": {
+          width: 30,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      },
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles(); 
 
   return (
-    <>
-    
-    </>
+    <Grid container className={classes.root}>
+      <Grid item xs={10} md={12} className={classes.table}>
+        <div className="headingRow">
+          <div className="head-cell">
+            <FormatListNumberedIcon />
+          </div>
+          <div>
+            <TextFieldsIcon />
+          </div>
+          <div className="head-cell">
+            <CheckIcon />
+          </div>
+          <div className="head-cell">
+            <DeleteForeverIcon />
+          </div>
+        </div>
+        <div className="list-block">
+
+        </div>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import App from "./App";
+import theme from "./theme";
+
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
